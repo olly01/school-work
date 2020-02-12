9797e310a0149612ac66e31f77f295ad34fa273b
Lillian Seaman <17U-LSeaman@cap.education>
2020-02-12 11:20:45 +0000

    animal

---
+++ school-work/Animal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace school_work
+{
+    class Animal
+    {
+        private String Name;
+        private Int64 Age;
+        private Boolean Sex;
+        private Int64 Health;
+    }
+}
