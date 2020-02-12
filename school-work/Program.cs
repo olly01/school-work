3efa349e79145982beadb820edbbc762a7e5ef9b
Pulley <17U-OPulley@cap.education>
2020-02-12 11:05:09 +0000

    Add project files.

---
+++ school-work/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace school_work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
