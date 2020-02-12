0de60bbc6750eabd6729359d0a4c4ff8e122631c
Joshua Marsh <josh@theswampies.co.uk>
2020-02-12 15:44:27 +0000

    Added base contructors

--- school-work/Ostrich.cs
+++ school-work/Ostrich.cs
@@ -6,7 +6,7 @@ namespace school_work
 {
     class Ostrich : Bird
     {
-        public Ostrich()
+        public Ostrich(string name, int age, Boolean sex, int health) : base(name, age, sex, health)
         {
             _diet = new List<string>() { "Roots", "Leaves", "Insects", "Seeds" };
         }
