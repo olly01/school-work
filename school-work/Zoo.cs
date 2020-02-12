fb594f625a5643f115a4f30197acb4391b38d972
Oliver Pulley <oliver.pulley01@gmail.com>
2020-02-12 15:30:10 +0000

    zoo

---
+++ school-work/Zoo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace school_work
+{
+    class Zoo
+    {
+
+        private List<Enclosures> enclosures = new list<Enclosures>(); 
+        private List<Animal> animals = new List<Animal>();
+        private List<Staff> _staff = new List<Staff>();
+
+        public List<Staff> Staff
+        {
+            get { return _staff = new List<Staff>(); }
+            set { _staff = new List<Staff>() = value; }
+        }
+
+
+    }
+}
