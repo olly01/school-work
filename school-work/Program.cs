09fc2bed80cfae1a0e54de246390972f09d76bf3
Charlotte Elliott <17U-CElliott@utccambridge.co.uk>
2020-03-03 11:13:15 +0000

    1

--- school-work/Program.cs
+++ school-work/Program.cs
@@ -8,7 +8,6 @@ namespace school_work
     {
         static void Main(string[] args)
         {
-            int bob = 10;
 
             List<Animal> animallist = new List<Animal>();
 
@@ -18,19 +17,6 @@ namespace school_work
             Console.WriteLine(fred.Name);
 
             Animal animal = new Animal("a", 1, true, 1);
-
-
-
-            Console.WriteLine(bob);
-            Console.ReadLine();
-
-            Penguin penguin = new Penguin();
-
-            foreach (String food in penguin.Diet)
-            {
-                Console.WriteLine(food);
-            }
-            Console.ReadLine();
         }
     }
 }
---
+++ school-work/Vet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace school_work
+{
+    class Vet : Staff
+    {
+        public void Treat(Animal reviveanimal)
+        {
+            reviveanimal.medicine();
+        }
+    }
+}
