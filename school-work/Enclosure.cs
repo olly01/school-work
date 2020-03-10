2098b7648bd780c9f34e87ae18c42142a7d621ca
Macmagdalena <17U-MMaciak@cap.education>
2020-03-10 11:15:07 +0000

    finally got it to sync to master

--- school-work/Enclosure.cs
+++ school-work/Enclosure.cs
@@ -6,27 +6,61 @@ namespace school_work
 {
     class Enclosure
     {
-        public List<Animal> animals = new List<Animal>();
-        public string _contents;
-        public int _size;
-        public int _WaterLevel;
-        public int _Temperature;
-        public bool _enclosed;
-        public int _PlantLife;
+        private List<Animal> animals = new List<Animal>();
+        private string enclosureName;
+        private int size;
+        private int capacity;
+        private int waterLevel;
+        private int temp;
+        private bool shelter;
+        private int plantLife;
 
-        public Enclosure(string contents, int size, int WaterLevel, int temperature, bool enclosed, int PlantLife)
+        public void set()
         {
-            this._contents = contents;
-            this._size = size;
-            this._WaterLevel = WaterLevel;
-            this._Temperature = temperature;
-            this._enclosed = enclosed;
-            this._PlantLife = PlantLife;
+            enclosureName = Convert.ToString(Console.ReadLine());
+            size = Convert.ToInt32(Console.ReadLine());
+            capacity = Convert.ToInt32(Console.ReadLine());
+            waterLevel = Convert.ToInt32(Console.ReadLine());
+            temp = Convert.ToInt32(Console.ReadLine());
+            shelter = Convert.ToBoolean(Console.ReadLine());
+            plantLife = Convert.ToInt32(Console.ReadLine());
+
         }
 
-        public void AddAnimal(Animal animal)
+        public void addAnimals(Animal _newAnimal)
         {
-            animals.Add(animal);
+            if (animals.Count < capacity)
+            {
+                animals.Add(_newAnimal);
+
+            }
+            else if (animals.Count >= capacity)
+            {
+                Console.WriteLine("Theres not enough space, please try another enclosure.");
+            }
+
+        }
+
+        public void removeAnimals(int _numberInList)
+        {
+            animals.RemoveAt(_numberInList);
+        }
+
+
+
+
+        public void Print()
+        {
+
+            Console.WriteLine("Enclosure: " + enclosureName);
+            Console.WriteLine("Size: " + size);
+            Console.WriteLine("Animal Capacity: " + capacity);
+            Console.WriteLine("Animals in the Enclosure:");
+            Console.WriteLine("Space Left: " + (size - animals.Count));
+            Console.WriteLine("Water Level: " + waterLevel);
+            Console.WriteLine("Temperature: " + temp);
+            Console.WriteLine("Shelter: " + shelter);
+            Console.WriteLine("Amount of Plant Life: " + plantLife);
         }
     }
 }
